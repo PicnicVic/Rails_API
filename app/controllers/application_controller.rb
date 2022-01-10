9dde6de5894c9341e28d221bb429f772fa51057f
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-01-10 18:54:54 +1100

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,64 @@
 class ApplicationController < ActionController::API
   # We will need this repeated functionlaity
   def authenticate
+      token = extract_token_from_auth_headers
+      payload = JwtServices.decode(token)
+
+      if payload
+        @current_user ||= User.find(payload["user_id"])
+      else
+        render json: {error: "You must be logged in to do that"}, status: 401
+      end
+      
   end
 
-  def current_user
+  def current_user 
+    @current_user
   end
 
   def logged_in?
+    !!@current_user
   end
 
   def extract_token_from_auth_headers
+    request.headers["Authorization"] && request.headers["Authorization"].split(" ")[1]
   end
 end
+# before_action :authorized
+
+#   def encode_token(payload)
+#     JWT.encode(payload, 'yourSecret')
+#   end
+
+#   def auth_header
+#     # { Authorization: 'Bearer <token>' }
+#     request.headers['Authorization']
+#   end
+
+#   def decoded_token
+#     if auth_header
+#       token = auth_header.split(' ')[1]
+#       # header: { 'Authorization': 'Bearer <token>' }
+#       begin
+#         JWT.decode(token, 'yourSecret', true, algorithm: 'HS256')
+#       rescue JWT::DecodeError
+#         nil
+#       end
+#     end
+#   end
+
+#   def logged_in_user
+#     if decoded_token
+#       user_id = decoded_token[0]['user_id']
+#       @user = User.find_by(id: user_id)
+#     end
+#   end
+
+#   def logged_in?
+#     !!logged_in_user
+#   end
+
+#   def authorized
+#     render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+#   end
+# end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe "Posts", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
+
+  describe "POST /posts" do
+    before(:all) do
+      @post_count = Post.count
+      @new_user = create(:user,email: "newuser@test.com", username: "Newuser")
+    end
+
+    context "with token" do
+      before(:each) do
+        token = JwtServices.encode(@new_user)
+        post "/posts", header: {Authorization: "Bearer #{token}"},params: {park_id: 1, comment: "I love this park", user_id: 1}
+      end
+
+      it "should respond with 201 created" do
+        expect(response).to have_http_status(201)
+      end  
+
+      it "should increase post count by 1" do
+        expect(Post.count).to be @post_count +1  
+      end
+
+      it "should contain the post content" do
+        expect(response.body).to include("I love this park")
+        expect(response.body).to include("Newuser")
+      end
+    end
+
 end
