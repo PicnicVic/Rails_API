444626edc9c00584e7f2e71ef7e0b2dc5c932927
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-01-12 17:32:10 +1100

    auto_park_id

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,42 +1,49 @@
 class ParksController < ApplicationController
   before_action :set_park, only: [:show, :update, :destroy]
   before_action :authenticate, only: [:create, :update, :destroy]
-  before_action :authorize, only: [:create, :update, :destroy]
+  before_action :authorize, only: [:update, :destroy]
 
   # Show all parks available
   def index
     parks = Park.all.includes(:category, :feature)
-    render json: parks, include: { category: { only: :name }, feature: { only: :name }, address: { only: [:number, :street, :suburb, :postcode] } }, status: 200
-    # render json: parks, include: ["category", "feature", "address"], status: 200
+    render json: parks, include: { category: { only: :name }, feature: { only: :name } }, status: 200
   end
 
   # Show individual Park, based on params[:id]
   def show
-    render json: @park, include: { category: { only: :name }, feature: { only: :name }, address: { only: [:number, :street, :suburb, :postcode] } }, status: 200
+    render json: @park, include: { category: { only: :name }, feature: { only: :name } }, status: 200
   end
 
   ### THESE CRUD FEATURES HAVE NOT BEEN TESTED
   def create
-    park = current_user.parks.create(park_params)
-    render_park(park)
+    if current_user.admin == true
+      park = current_user.parks.create(park_params)
+      render_park(park)
+    else
+      render json: { error: "You are not authorised to do that" }, status: 404
+    end
   end
 
   def update
-    @park.update(park_params)
-    render_park(@park)
+    if current_user.admin == true
+      @park.update(park_params)
+      render_park(@park)
+    end
   end
 
   def destroy
-    attributes = @park.attributes
-    @park.destroy
-    render json: attributes, status: 202
+    if current_user.admin == true
+      attributes = @park.attributes
+      @park.destroy
+      render json: attributes, status: 202
+    end
   end
 
   private
 
   def set_park
     begin
-      @park = Park.find(params[:id])
+      @park = Park.find(params[:park_id])
     rescue
       # Will run if exception is raised
       render json: { error: "Could not find this park" }, status: 404
@@ -49,7 +56,7 @@ class ParksController < ApplicationController
 
   def authorize
     # Permission for user to edit/update post
-    render json: { error: "You do not have permission to do that" }, status: 401 unless current_user.id == 1
+    render json: { error: "You do not have permission to do that" }, status: 401 unless current_user.admin == true
   end
 
   def render_park(park)
