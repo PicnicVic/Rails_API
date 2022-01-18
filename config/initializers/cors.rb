06baf3a1965aff09fb27dedc8f7138147725ac60
“Karla <karlamariztolentino@gmail.com>
2022-01-18 17:15:28 +1100

    merged cors.rb conflict with main repo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,14 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-<<<<<<< HEAD
     origins "http://localhost:3001"
-=======
-    origins "http://localhost:3001", "http://localhost:3002", "https://picnic-vic.netlify.app"
-    # origins "http://localhost:3002"
->>>>>>> b6b107949123c565217097f247ec8a786e1ae048
     # origins "*"
-
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
